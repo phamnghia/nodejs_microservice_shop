2d0f8773c68ea3a63278a19e491d00e9fe0b9505
phamnghia <nghiapd92@gmail.com>
2018-04-19 10:41:44 +0700

    add service

--- product_service/index.js
+++ product_service/index.js
@@ -7,7 +7,7 @@ const app = express();
 const Product = require("./models/product");
 
 // Running app
-console.log(`Product Service - Tag: ${process.env.DOCKER_TAG}`)
+console.log(`Product Service - Tag: ${process.env.DOCKER_TAG} | Service name: ${process.env.SERVICE_NAME}`);
 mongoose.connect(`mongodb://${process.env.MONGODB_HOST}:${process.env.MONGODB_PORT}/${process.env.MONGODB_DATABASE_NAME}`)
 	.then(() => {
 		app.listen(80, () => console.log("Product service is running on port 80"));
